57c8bfff6e4c14b28d7b90135a62fc06b44ddda4
Kai Takami <kaitakami@protonmail.com>
2022-09-05 21:27:28 +0900

    Add travels data and render it in app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import Travel from "./components/Travel";
+import data from "./data";
 
 function App() {
+  const travels = data.map((travel) => (
+    <Travel key={travel.mapsUrl} {...travel} />
+  ));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a
+        className="website-refferal"
+        href="https://kaitakami.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Navbar />
+      </a>
+      <div className="container">{travels}</div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,43 @@
+const data = [
+  {
+    imgAlt: "Mount Fuji image",
+    imgUrl:
+      "https://images.unsplash.com/photo-1589308078059-be1415eab4c3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYzNzMzOTgxMA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080",
+    country: "Japan",
+    mapsUrl:
+      "https://www.google.com/maps/place/Mount+Fuji/@35.3606422,138.7185872,15z/data=!4m5!3m4!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634",
+    name: "Mount Fuji",
+    startDate: "20 Sep, 2022",
+    endDate: "22 Sep, 2022",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+  },
+  {
+    imgAlt: "Tottori beach image",
+    imgUrl:
+      "https://images.unsplash.com/photo-1635902918331-10e4952b3920?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    country: "Japan",
+    mapsUrl:
+      "https://www.google.com/maps/place/Tottori/@35.4226245,134.0533178,11z/data=!3m1!4b1!4m5!3m4!1s0x355596c9a0846f89:0x3847638629e55456!8m2!3d35.5011082!4d134.2351011",
+    name: "Tottori",
+    startDate: "17 Aug, 2022",
+    endDate: "18 Aug, 2022",
+    description:
+      "Tottori is the capital, and the largest city in Japan's Tottori Prefecture in the Chūgoku region of Japan. Within Japan the city is best known for its sand dunes which are a popular tourist attraction, drawing visitors from outside the prefecture.",
+  },
+  {
+    imgAlt: "Mazatlan beach image",
+    imgUrl:
+      "https://images.unsplash.com/photo-1612086730392-be7864e4ea65?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    country: "Mexico",
+    mapsUrl:
+      "https://www.google.com/maps/place/Mazatl%C3%A1n,+Sinaloa,+Mexico/@23.2468861,-106.4923172,12z/data=!3m1!4b1!4m5!3m4!1s0x869f5341681837c1:0x62a4c4aaf13c72b2!8m2!3d23.2494148!4d-106.4111425",
+    name: "Mazatlan",
+    startDate: "18 Jun, 2021",
+    endDate: "24 Jun, 2021",
+    description:
+      "Mazatlán is a Mexican resort town along the Pacific shoreline in the state of Sinaloa. Sandy beaches line its 21km-long malecón (boardwalk), and it's renowned for big-game fishing. In its Centro Histórico, or Old Mazatlán, 19th-century landmarks include the performance hall Teatro Ángela Peralta and the towering Immaculate Conception basilica. The modern district of Zona Dorada is known for nightlife and hotels.",
+  },
+];
+
+export default data;
